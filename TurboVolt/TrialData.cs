d8c72b2fffcc95b0495674f28b4a8659be62bed6
67676kidkid <gusinacvahid7@gmail.com>
2026-07-26 21:47:51 +0200

    Add TurboVolt/TrialData.cs

---
+++ TurboVolt/TrialData.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TurboVolt;
+
+public class TrialData
+{
+	public DateTime Issued { get; set; }
+
+	public DateTime Expires { get; set; }
+
+	public string MachineId { get; set; } = "";
+
+}
